40a801a44f2c60eb1e88734405685ba6ad7ff5ac
RScott99 <rsh06.rh@gmail.com>
2025-07-23 21:20:12 -0400

    finished code and table looks to be correct

--- Chapter 6 Programming Project.cpp
+++ Chapter 6 Programming Project.cpp
@@ -10,12 +10,32 @@ const int maxFahrenheit = 20;
 
 //defining function prototype
 
-double getCelsius
+double getCelsius(int fahrenheit);
+//should convert a temp in fahrenheit into celsius
+//inputs an integer that represents the temp in fahrenheit
+//returns the equivalent temperature in celsius
 
 
 
 int main()
 {
-    
+
+    //Display title and headers of table
+    std::cout << "Celsius Temp. Conversion Table\n";
+    std::cout << setw(10) << "Fahrenheit" << setw(15) << "Celsius\n";
+    std::cout << "-------------------------------\n";
+
+    //Display rows of table
+    for (int j = minFahrenheit; j <= maxFahrenheit; ++j) {
+        double celsius = getCelsius(j);
+        std::cout << fixed << setprecision(1);
+        std::cout << setw(10) << j << setw(15) << celsius << endl;
+
+    }
     return 0;
+}
+
+//defining function
+double getCelsius(int fahrenheit) {
+    return ((5.0 / 9.0) * (fahrenheit - 32));
 }
