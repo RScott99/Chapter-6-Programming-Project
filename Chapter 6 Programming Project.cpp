ab4bd9e9bbb03f43b874bb2db379e9620223a621
RScott99 <rsh06.rh@gmail.com>
2025-07-23 21:13:27 -0400

    began code and renamed branch

--- Chapter 6 Programming Project.cpp
+++ Chapter 6 Programming Project.cpp
@@ -1,6 +1,16 @@
+//should create a table showing the conversion between celsius and fahrenheit temps between 0 and 20 degrees.
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
+
+//defining global constants
+const int minFahrenheit = 0;
+const int maxFahrenheit = 20;
+
+//defining function prototype
+
+double getCelsius
 
 
 
